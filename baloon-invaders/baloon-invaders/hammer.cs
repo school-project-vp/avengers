522c5bddbfa6c96f57e47196371d6fc2bef13ff2
Gligor23 <glgudjevski@gmail.com>
2019-07-02 23:29:42 +0200

    merge thor

---
+++ baloon-invaders/baloon-invaders/hammer.cs
@@ -0,0 +1,41 @@
+﻿using baloon_invaders.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baloon_invaders
+{
+    public class hammer
+    {
+        public Point position { get; set; }
+        public Image img { get; set; }
+
+        public hammer(Point p)
+        {
+            position = p;
+            img = Resources.thor_hammer;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawImageUnscaled(img, position);
+        }
+
+        public void Move()
+        {
+            position = new Point(position.X, position.Y - 20);
+        }
+
+        public bool IsGone()
+        {
+            if (position.Y + 20 < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
